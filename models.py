6f683f9cc9f6836afd8f1e078540eef1ab0ad404
Denis Mashtak <127735395+SaL1VaNn@users.noreply.github.com>
2023-09-30 15:48:33 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,38 +1,60 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, Date
-from sqlalchemy.orm import relationship
+import datetime
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table, DateTime, Date
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, Mapped
+from sqlalchemy.orm import DeclarativeBase
+import psycopg2
+
+DATABASE_URL = f"postgresql+psycopg2://postgres:0000@db:5432/postgres" 
+
+
+engine = create_engine(DATABASE_URL, echo=True)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 Base = declarative_base()
 
-class Student(Base):
-    __tablename__ = 'students'
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
 
 class Group(Base):
     __tablename__ = 'groups'
-    
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-  
+    name = Column(String(20), nullable=False)
+
+
+class Person():
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+
+
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    fullname = Column(String(120), nullable=False)
+    group_id = Column('group_id', ForeignKey('groups.id', ondelete='CASCADE'))
+    group = relationship('Group', backref='students')
+
+ 
 class Teacher(Base):
     __tablename__ = 'teachers'
-
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    fullname = Column(String(120), nullable=False)
 
-class Subject(Base):
-    __tablename__ = 'subjects'
+class Discipline(Base):
+    __tablename__ = 'disciplines'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    teacher_id = Column(Integer, ForeignKey('teachers.id'))
-    teacher = relationship('Teacher')
+    name = Column(String(120), nullable=False)
+    teacher_id = Column('teacher_id', ForeignKey('teachers.id', ondelete='CASCADE'))
+    teacher = relationship('Teacher', backref='disciplines')
+
 
 class Grade(Base):
     __tablename__ = 'grades'
     id = Column(Integer, primary_key=True)
-    student_id = Column(Integer, ForeignKey('students.id'))
-    subject_id = Column(Integer, ForeignKey('subjects.id'))
-    score = Column(Float)
-    date = Column(Date)  
+    grade = Column(Integer, nullable=False)
+    date_of = Column('date_of', Date, nullable=True)
+    student_id = Column('student_id', ForeignKey('students.id', ondelete='CASCADE'))
+    discipline_id = Column('discipline_id', ForeignKey('disciplines.id', ondelete='CASCADE'))
+    student = relationship('Student', backref='grade')
+    discipline = relationship('Discipline', backref='grade')
+    
